f2a72979c6d86a8d3fe661c2b7dded18fdf70222
Marek Wasyluk <marekwasyluk@wp.pl>
2022-11-16 21:54:59 +0100

    feat: connect to the server over web socket feat: send messages to the server and listen for messages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import useWindowDimensions from './utils/window-resizer';
+import WebSocketService from './components/WebSocketService';
 
 function App() {
   const { height, width } = useWindowDimensions();
   const margin = 10;
+
   return (
     <div className="App" style={{
       margin: margin + "px",
@@ -11,8 +13,9 @@ function App() {
       height: height - 2 * margin,
     }}>
 
-      <h1 style={{ backgroundColor: "lightblue" }}>OuRoom!</h1>
 
+      <h1 style={{ backgroundColor: "lightblue" }}>OuRoom!</h1>
+      <WebSocketService userId={"randomID"}></WebSocketService>
     </div >
   );
 }
