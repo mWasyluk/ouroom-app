9347bf808aacd14d7b18393187a5d456bf69b706
Marek Wasyluk <marekwasyluk@wp.pl>
2022-12-17 19:00:15 +0100

    feat: add avatar and name props to the Conversation domain

--- src/domains/Conversation.js
+++ src/domains/Conversation.js
@@ -1,6 +1,22 @@
+import avatar from "../resources/wip-conversation.png"
+
 export default class Conversation {
     constructor(conversation) {
         this.id = conversation.id;
         this.participators = conversation.participators;
+        this.name = this.#getNameByParticipators(conversation.participators);
+        this.avatarUrl = avatar;
+        this.messages = conversation.messages;
+    }
+
+    #getNameByParticipators = function (participators = []) {
+        let name = '';
+
+        for (let i = 0; i < participators.length - 1; i++) {
+            name += participators[i].firstName + ', ';
+        }
+
+        name += participators[participators.length - 1].firstName;
+        return name;
     }
 }
