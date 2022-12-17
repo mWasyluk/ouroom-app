84bedbc74c8aaee4d04fde3c8645e9e9a17bb1ed
Marek Wasyluk <marekwasyluk@wp.pl>
2022-12-17 18:43:35 +0100

    feat: fetch Convesations when the App is starting feat: fetch Messages when Conversation is selected

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import { Component } from "react";
 import Chat from './Chat';
 import ConversationsList from './ConversationsList';
-import { getMessagesWith, getUsernameById } from '../utils/fetch'
+import { getConversationMessages, getUserConversations } from '../utils/fetch'
 
 export default class App extends Component {
     constructor(props) {
@@ -12,40 +12,39 @@ export default class App extends Component {
             user: props.user,
             setUser: props.setUser,
             setUserStatus: props.setUserStatus,
-            targetFriend: null
+            targetConversation: null
         }
+
+        getUserConversations().then(conversations =>
+            this.setState({ conversations: conversations })
+        );
     }
 
-    async handleFriendSelection(friend) {
-        let messagess = await getMessagesWith(this.state.user.id, friend.id);
-        if (!this.state.conversations.some((con) => con.friend === friend)) {
-            this.setState({
-                conversations: [{ friend: friend, messages: messagess }, ...this.state.conversations]
-            })
-        }
-        this.setState({
-            targetFriend: friend
-        })
-
+    async handleConversationSelection(conversation) {
+        let messages = await getConversationMessages(conversation.id);
+        let target = this.state.conversations
+            .filter(conv => conv.id === conversation.id)[0]
+        target.messages = messages;
+        this.setState({ targetConversation: target })
     }
 
     login = async (e) => {
         e.preventDefault();
 
-        const uuidRegex = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
-        const userId = e.target[0].value
+        // const uuidRegex = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+        // const userId = e.target[0].value
 
-        if (uuidRegex.test(userId)) {
-            let username = await getUsernameById(userId)
-            let user = {
-                id: userId,
-                name: username
-            }
-            this.setState({ user: user })
-            this.state.setUser(user);
-        } else {
-            throw new Error("Wprowadzono nieprawidłowy UUID")
-        }
+        // if (uuidRegex.test(userId)) {
+        //     let username = await getUsernameById(userId)
+        //     let user = {
+        //         id: userId,
+        //         name: username
+        //     }
+        //     this.setState({ user: user })
+        //     this.state.setUser(user);
+        // } else {
+        //     throw new Error("Wprowadzono nieprawidłowy UUID")
+        // }
     }
 
     render() {
@@ -61,8 +60,8 @@ export default class App extends Component {
 
         const logged = (
             <div className="logged" >
-                <ConversationsList select={this.handleFriendSelection.bind(this)} />
-                <Chat user={this.state.user} target={this.state.targetFriend} conversations={this.state.conversations} setUserStatus={this.state.setUserStatus}></Chat>
+                <ConversationsList conversations={this.state.conversations} select={this.handleConversationSelection.bind(this)} />
+                <Chat user={this.state.user} conversation={this.state.targetConversation} setUserStatus={this.state.setUserStatus}></Chat>
             </div >
         )
 
