611ced74ccb538bbed0336a3bc159f1ec96b7a96
Marek Wasyluk <marekwasyluk@wp.pl>
2022-12-18 17:11:19 +0100

    refactor: move Stomp connection handling functions to an external file

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,34 +1,16 @@
 import './Chat.css'
-import SockJS from 'sockjs-client'
-import { Stomp } from '@stomp/stompjs'
 import React from 'react';
 import { AiOutlineSend } from 'react-icons/ai';
-import Message from '../domains/Message';
 import { postConversationMessage } from '../utils/fetch';
 
-// const serverHost = 'localhost'
-const serverHost = '192.168.0.24'
-const serverUrl = 'http://' + serverHost + ':8080/ouroom';
-
 export default class Chat extends React.Component {
-    stompClient;
-
     constructor(props) {
         super(props)
 
         this.state = {
             user: props.user,
-            setUserStatus: props.setUserStatus,
-            isConnected: false,
             conversation: props.conversation
         }
-
-        this.connectStomp()
-    }
-
-    componentDidUpdate() {
-        if (this.state.isConnected) this.state.setUserStatus('online')
-        else this.state.setUserStatus('offline')
     }
 
     static getDerivedStateFromProps(nextProps, prevState) {
@@ -39,67 +21,7 @@ export default class Chat extends React.Component {
         return null
     }
 
-    connectStomp() {
-        this.stompClient = Stomp.over(() => new SockJS(serverUrl));
-        this.stompClient.debug = () => { }
-
-        this.stompClient.connect(
-            {},
-            () => this.stompOnConnectCallBack(),
-            (err) => this.stompErrorCallBack(err)
-        );
-        this.stompClient.onWebSocketClose = this.stompOnCloseCallBack
-    }
-
-    stompOnConnectCallBack() {
-        console.log("WebSocket is connected.")
-        this.setState({ isConnected: true });
-        this.stompClient.subscribe(
-            // '/conversations.' + this.state.user.id,
-            '/topic/03a3c0d9-23ec-4fb1-9ddc-b927bf341381',
-            (message) => this.stompSubscriptionCallBack(message));
-    }
-
-    stompOnCloseCallBack = () => {
-        setTimeout(() => {
-            this.setState({ isConnected: false });
-            console.log("Connection has been closed.", '\nReconnecting...');
-            this.connectStomp();
-        }, 2500);
-    }
-
-    isIncomming = (message) => {
-        return message.sender !== this.state.user.id
-    }
-
-    async stompSubscriptionCallBack(subscriptionMessage) {
-        if (subscriptionMessage.body) {
-            let message = new Message(JSON.parse(subscriptionMessage.body));
-
-            // TODO: send to correct conversation if its different than selected
-            this.state.conversation.messages.unshift(message);
-            this.forceUpdate();
-        }
-    }
-
-
-    stompErrorCallBack(err) {
-        setTimeout(() => {
-            console.log("Connection error: ", err.reason, '\nReconnecting...');
-            this.connectStomp();
-        }, 2000);
-    }
-
-    handleConnectToChannel(e) {
-        e.preventDefault();
-
-        let target = e.target[0].value;
-
-        this.connectStomp(target);
-    }
-
-    // TODO: Check if stompClient is null
-    handleSendMessage(e) {
+    sendMessage(e) {
         e.preventDefault();
 
         let content = e.target[0].value
@@ -117,20 +39,18 @@ export default class Chat extends React.Component {
 
     render() {
         let conversation = this.state.conversation;
-        let messagesList = null
+        let messagesList = []
         if (conversation) {
-            let messages = conversation ? conversation.messages : null
-            messagesList = messages.map(message => {
+            messagesList = conversation.messages.map(message => {
                 let positionClass = message.sender === this.state.user.id ?
                     'sent' : 'received'
                 return <div className={positionClass} key={message.id}>{message.content}</div>
-            }
-            )
+            });
         }
 
         return (
             <div className='chat'>
-                <form className='content-bar' onSubmit={this.handleSendMessage.bind(this)}>
+                <form className='content-bar' onSubmit={this.sendMessage.bind(this)}>
                     <input className='content' type="text" placeholder="Treść wiadomości" />
                     <button className='send' type="submit"><AiOutlineSend size={'2.5em'} /></button>
                 </form>
---
+++ src/utils/websocket.js
@@ -0,0 +1,74 @@
+import SockJS from 'sockjs-client'
+import { Stomp } from '@stomp/stompjs'
+import React, { Component } from 'react';
+import Message from '../domains/Message';
+
+const serverHost = '192.168.0.24'
+const serverUrl = 'http://' + serverHost + ':8080/ouroom';
+
+export default class WebSocketConnection extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            topicId: props.topicId,
+            subscriptionCallback: props.subscriptionCallback,
+            statusChangeCallback: props.statusChangeCallback
+        }
+    }
+
+    componentDidMount() {
+        this.connectStomp();
+    }
+
+    connectStomp = () => {
+        this.stompClient = Stomp.over(() => new SockJS(serverUrl));
+        this.stompClient.debug = () => { }
+
+        this.stompClient.connect(
+            {},
+            () => this.stompOnConnectCallBack(),
+            (err) => this.stompErrorCallBack(err)
+        );
+        this.stompClient.onWebSocketClose = this.stompOnCloseCallBack
+    }
+
+    stompOnConnectCallBack = () => {
+        console.log("WebSocket is connected.")
+        this.state.statusChangeCallback(true)
+        this.stompClient.subscribe(
+            '/topic/' + this.state.topicId,
+            (message) => this.state.subscriptionCallback(message)
+        );
+    }
+
+    stompOnCloseCallBack = () => {
+        setTimeout(() => {
+            this.state.statusChangeCallback(false)
+            console.log("Connection has been closed.", '\nReconnecting...');
+            this.connectStomp();
+        }, 2500);
+    }
+
+    // async stompSubscriptionCallBack(subscriptionMessage) {
+    //     if (subscriptionMessage.body) {
+    //         let message = new Message(JSON.parse(subscriptionMessage.body));
+
+    //         // TODO: send to correct conversation if its different than selected
+    //         this.state.conversation.messages.unshift(message);
+    //         this.forceUpdate();
+    //     }
+    // }
+
+
+    stompErrorCallBack = (err) => {
+        setTimeout(() => {
+            this.state.statusChangeCallback(false)
+            console.log("Connection error: ", err.reason, '\nReconnecting...');
+            this.connectStomp();
+        }, 2500);
+    }
+
+    render() {
+        return <></>
+    }
+}
