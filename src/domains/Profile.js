1058c5a5bdd092fa16a4eb5019e2fca95a2f528d
Marek Wasyluk <marekwasyluk@wp.pl>
2022-12-19 02:26:16 +0100

    refactor: liken the Profile domain to the API response

--- src/domains/Profile.js
+++ src/domains/Profile.js
@@ -1,7 +1,7 @@
 export default class Profile {
     constructor(profile) {
         this.id = profile.id;
-        this.avatarUrl = profile.avatar.imageUrl;
+        this.avatar = profile.avatar;
         this.lastName = profile.lastName;
         this.firstName = profile.firstName;
     }
