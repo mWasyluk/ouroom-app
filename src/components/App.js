c3ebeb8362a0bfbf735c020a5722f7b577e22241
Marek Wasyluk <marekwasyluk@wp.pl>
2022-12-19 02:23:55 +0100

    refactor: move the LoginForm component to the Root

--- src/components/App.js
+++ src/components/App.js
@@ -6,30 +6,21 @@ import { getConversationMessages, getUserConversations } from '../utils/fetch'
 import '../utils/websocket.js'
 import WebSocketConnection from '../utils/websocket.js';
 import Message from '../domains/Message';
-import LoginForm from './LoginForm';
 
 export default class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            conversations: [],
             user: props.user,
-            setUser: props.setUser,
-            authToken: '',
             setUserStatus: props.setUserStatus,
+            authToken: props.authToken,
+            conversations: [],
             targetConversation: null
         }
 
-        getUserConversations().then(conversations =>
+        getUserConversations(this.state.authToken).then(conversations =>
             this.setState({ conversations: conversations })
         );
-    }
-
-    static getDerivedStateFromProps(nextProps, prevState) {
-        if (nextProps.user !== prevState.user) {
-            return ({ user: nextProps.user })
-        }
-        return null
     }
 
     subsciptionCallback = (plainMessage) => {
@@ -48,42 +39,21 @@ export default class App extends Component {
     }
 
     async handleConversationSelection(conversation) {
-        let messages = await getConversationMessages(conversation.id);
+        let messages = await getConversationMessages(this.state.authToken, conversation.id);
         let target = this.state.conversations
             .filter(conv => conv.id === conversation.id)[0]
         target.messages = messages;
         this.setState({ targetConversation: target })
     }
 
-    setAccount = (account) => {
-        this.state.setUser(account);
-    }
-
-    setAuthToken = (token) => {
-        this.setState({ authToken: token })
-    }
-
     render() {
-        let view;
-        if (!this.state.user)
-            view = (
-                <div className="login">
-                    <LoginForm setAccount={this.setAccount} setAuthToken={this.setAuthToken} />
-                </div>
-            )
-        else
-            view = (
-                <div className="logged" >
-                    <WebSocketConnection topicId={this.state.user.profile.id} subscriptionCallback={this.subsciptionCallback.bind(this)} statusChangeCallback={this.connectionStatusChangeCallback.bind(this)} />
-                    <ConversationsList conversations={this.state.conversations} select={this.handleConversationSelection.bind(this)} />
-                    <Chat user={this.state.user} conversation={this.state.targetConversation}></Chat>
-                </div >
-            )
-
         return (
-            <div className="App">
-                {view}
-            </div>
+            <div className="logged">
+                <WebSocketConnection topicId={this.state.user.profile.id} subscriptionCallback={this.subsciptionCallback.bind(this)} statusChangeCallback={this.connectionStatusChangeCallback.bind(this)} />
+                <ConversationsList conversations={this.state.conversations} select={this.handleConversationSelection.bind(this)} />
+                <Chat user={this.state.user} conversation={this.state.targetConversation} authToken={this.state.authToken}></Chat>
+            </div >
+
         )
     }
 }
