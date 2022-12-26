4b996c59960754c98e8e6a5388c255926d026709
Marek Wasyluk <marekwasyluk@wp.pl>
2022-12-26 15:58:32 +0100

    fix: remember me cannot be unchecked

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -24,12 +24,16 @@ const LoginForm = (props) => {
         }
     }
 
+    const handleRememberMe = () => {
+        setRememberMe(!rememberMe);
+    }
+
     return (
         <div className="popup-form">
             <span>Zaloguj się do<strong>{appTitle}!</strong></span>
             <input type="text" name="email" id="email" placeholder="E-mail..." />
             <input type="password" name="password" id="password" placeholder="Hasło..." />
-            <label id='remember-me'> <input type="checkbox" checked={rememberMe} onChange={setRememberMe} /> Zapamiętaj mnie </label>
+            <label id='remember-me'> <input type="checkbox" checked={rememberMe} onChange={handleRememberMe} /> Zapamiętaj mnie </label>
             <button type='submit' onClick={handleSubmit} id="submit">Zaloguj się</button>
             <p className='footer-text'>
                 Nie masz jeszcze konta?
