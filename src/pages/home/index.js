449047547d8b836b46621cf79c60348cbcdc9457
mWasyluk <marekwasyluk@wp.pl>
2023-12-18 22:56:45 +0100

    style: append new line at the eof

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,6 @@
 import './style.css';
 
-import { useLocation, useNavigate } from 'react-router';
-
 function HomePage() {
-    const navigate = useNavigate();
-    const location = useLocation();
-
     return (
         <div className='home'>
             home page
